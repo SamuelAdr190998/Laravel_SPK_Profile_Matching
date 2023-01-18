6a6e0ab78f854dcd28b8f16f1522e4981bcbb2ea
SamuelNusanetID <samuel@nusa.net.id>
2023-01-18 22:36:12 +0700

    Perbaikan Home Blade

--- resources/views/guest/pages/home.blade.php
+++ resources/views/guest/pages/home.blade.php
@@ -3,18 +3,27 @@
 @section('content-wrapper')
     <div class="card-body bg-white pb-5" id="card-body-wrapper">
         <h2 class="text-center fw-bold mb-5 mt-3">Beranda</h2>
-        <div class="container py-5">
+        <div class="container" style="padding-bottom: 30em;">
             <div class="d-flex align-items-center justify-content-center" style="height: 15em;">
-                <div class="container-fluid row text-center">
-                    <h5 class="fw-bold mb-3">
-                        Sistem Pendukung Pengambilan Keputusan Rekomendasi Rumah Kos Dengan Menggunakan Metode Multi
-                        Attribute Utility Theory (MAUT) Berbasis Web
-                    </h5>
-                    <p>
-                        Multi Attribute Utility Theory (MAUT) merupakan suatu metode perbandingan kuantitatif yang
-                        biasanya mengkombinasikan pengukuran atas biaya resiko dan keuntungan yang berbeda. Setiap
-                        kriteria yang ada memiliki beberapa alternatif yang mampu memberikan solusi.
-                    </p>
+                <div class="container-fluid row">
+                    <div class="col-sm-6" style="padding-top: 30em;">
+                        <ul class="ps-0 ps-0 fw-bold" style="list-style: none;">
+                            <li class="h3">Mau cari kos tapi bingung mau tanya ke siapa?</li>
+                            <li class="h5">Belum punya kos tapi ga sempat mau cari karena sibuk nugas atau kerja?</li>
+                            <li class="h4">Udah nemu kos tapi gatau fasilitas dan peraturannya apa aja?</li>
+                        </ul>
+                        <p class="mb-0">
+                            Cobain deh aplikasi KAU (Kos Area USU) ini!
+                        </p>
+                        <p style="text-align: justify;">
+                            Kos Area USU dirancang untuk membantu kamu memilih rumah kos berdasarkan kriteria kos yang kamu
+                            cari dan didesain sedemikian rupa agar kamu mudah menggunakannya.
+                        </p>
+                    </div>
+                    <div class="col-sm-6 d-flex justify-content-center">
+                        <img class="img-fluid bg-success bg-gradient rounded"
+                            src="{{ URL::to('library/simpletemp/img/Untitled design.png') }}" alt="home-img" width="350">
+                    </div>
                 </div>
             </div>
         </div>
